06261af5f980a9ffb013b5a6f09b1e204a829832
KuuKuu <42238907+VODINHQUOC@users.noreply.github.com>
2026-07-22 13:54:23 +0700

    test: isolate consumer Eventuate transport

--- consumer-service/src/test/java/net/ftgo/consumer/config/EventuateTramTestConfiguration.java
+++ consumer-service/src/test/java/net/ftgo/consumer/config/EventuateTramTestConfiguration.java
@@ -2,6 +2,7 @@ package net.ftgo.consumer.config;
 
 import io.eventuate.tram.commands.producer.CommandProducer;
 import io.eventuate.tram.events.publisher.DomainEventPublisher;
+import io.eventuate.tram.messaging.consumer.MessageConsumer;
 import io.eventuate.tram.messaging.producer.MessageProducer;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
@@ -15,19 +16,25 @@ import static org.mockito.Mockito.mock;
  */
 @TestConfiguration
 public class EventuateTramTestConfiguration {
-    
+
     @Bean
     @Primary
     public MessageProducer messageProducer() {
         return mock(MessageProducer.class);
     }
-    
+
+    @Bean
+    @Primary
+    public MessageConsumer messageConsumer() {
+        return mock(MessageConsumer.class);
+    }
+
     @Bean
     @Primary
     public DomainEventPublisher domainEventPublisher() {
         return mock(DomainEventPublisher.class);
     }
-    
+
     @Bean
     @Primary
     public CommandProducer commandProducer() {
