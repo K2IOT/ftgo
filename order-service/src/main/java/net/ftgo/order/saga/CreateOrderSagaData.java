4fea4532764a1d550efd8bc6be3b18e97f01dcd1
KuuKuu <42238907+VODINHQUOC@users.noreply.github.com>
2026-07-23 14:15:06 +0700

    fix: preserve empty create saga data compatibility

--- order-service/src/main/java/net/ftgo/order/saga/CreateOrderSagaData.java
+++ order-service/src/main/java/net/ftgo/order/saga/CreateOrderSagaData.java
@@ -16,12 +16,12 @@ public class CreateOrderSagaData {
     private Long orderId;
     private Long consumerId;
     private Long restaurantId;
-    private List<OrderLineItem> lineItems = new ArrayList<>();
+    private List<OrderLineItem> lineItems;
     private Money orderTotal;
 
     private Long expectedMenuVersion = 0L;
-    private List<OrderMenuLineItem> requestedMenuItems = new ArrayList<>();
-    private List<OrderMenuLineItem> authoritativeMenuItems = new ArrayList<>();
+    private List<OrderMenuLineItem> requestedMenuItems;
+    private List<OrderMenuLineItem> authoritativeMenuItems;
     private Money authoritativeTotal;
 
     private Long creditReservationId;
@@ -70,132 +70,50 @@ public class CreateOrderSagaData {
                 item.getQuantity()
             ))
             .toList();
+        this.authoritativeMenuItems = new ArrayList<>();
         this.acceptanceDeadline = LocalDateTime.now().plusMinutes(5);
     }
 
-    public Long getOrderId() {
-        return orderId;
-    }
-
-    public void setOrderId(Long orderId) {
-        this.orderId = orderId;
-    }
-
-    public Long getConsumerId() {
-        return consumerId;
-    }
-
-    public void setConsumerId(Long consumerId) {
-        this.consumerId = consumerId;
-    }
-
-    public Long getRestaurantId() {
-        return restaurantId;
-    }
-
-    public void setRestaurantId(Long restaurantId) {
-        this.restaurantId = restaurantId;
-    }
-
-    public List<OrderLineItem> getLineItems() {
-        return lineItems;
-    }
-
+    public Long getOrderId() { return orderId; }
+    public void setOrderId(Long orderId) { this.orderId = orderId; }
+    public Long getConsumerId() { return consumerId; }
+    public void setConsumerId(Long consumerId) { this.consumerId = consumerId; }
+    public Long getRestaurantId() { return restaurantId; }
+    public void setRestaurantId(Long restaurantId) { this.restaurantId = restaurantId; }
+    public List<OrderLineItem> getLineItems() { return lineItems; }
     public void setLineItems(List<OrderLineItem> lineItems) {
         this.lineItems = lineItems == null ? new ArrayList<>() : new ArrayList<>(lineItems);
     }
-
-    public Money getOrderTotal() {
-        return authoritativeTotal != null ? authoritativeTotal : orderTotal;
-    }
-
-    public void setOrderTotal(Money orderTotal) {
-        this.orderTotal = orderTotal;
-    }
-
-    public Long getExpectedMenuVersion() {
-        return expectedMenuVersion;
-    }
-
-    public void setExpectedMenuVersion(Long expectedMenuVersion) {
-        this.expectedMenuVersion = expectedMenuVersion;
-    }
-
-    public List<OrderMenuLineItem> getRequestedMenuItems() {
-        return requestedMenuItems;
-    }
-
+    public Money getOrderTotal() { return authoritativeTotal != null ? authoritativeTotal : orderTotal; }
+    public void setOrderTotal(Money orderTotal) { this.orderTotal = orderTotal; }
+    public Long getExpectedMenuVersion() { return expectedMenuVersion; }
+    public void setExpectedMenuVersion(Long expectedMenuVersion) { this.expectedMenuVersion = expectedMenuVersion; }
+    public List<OrderMenuLineItem> getRequestedMenuItems() { return requestedMenuItems; }
     public void setRequestedMenuItems(List<OrderMenuLineItem> requestedMenuItems) {
         this.requestedMenuItems = requestedMenuItems == null
             ? new ArrayList<>()
             : new ArrayList<>(requestedMenuItems);
     }
-
-    public List<OrderMenuLineItem> getAuthoritativeMenuItems() {
-        return authoritativeMenuItems;
-    }
-
+    public List<OrderMenuLineItem> getAuthoritativeMenuItems() { return authoritativeMenuItems; }
     public void setAuthoritativeMenuItems(List<OrderMenuLineItem> authoritativeMenuItems) {
         this.authoritativeMenuItems = authoritativeMenuItems == null
             ? new ArrayList<>()
             : new ArrayList<>(authoritativeMenuItems);
     }
-
-    public Money getAuthoritativeTotal() {
-        return authoritativeTotal;
-    }
-
-    public void setAuthoritativeTotal(Money authoritativeTotal) {
-        this.authoritativeTotal = authoritativeTotal;
-    }
-
-    public Long getCreditReservationId() {
-        return creditReservationId;
-    }
-
-    public void setCreditReservationId(Long creditReservationId) {
-        this.creditReservationId = creditReservationId;
-    }
-
-    public Long getTicketId() {
-        return ticketId;
-    }
-
-    public void setTicketId(Long ticketId) {
-        this.ticketId = ticketId;
-    }
-
-    public Long getAuthorizationId() {
-        return authorizationId;
-    }
-
-    public void setAuthorizationId(Long authorizationId) {
-        this.authorizationId = authorizationId;
-    }
-
-    public LocalDateTime getAcceptanceDeadline() {
-        return acceptanceDeadline;
-    }
-
-    public void setAcceptanceDeadline(LocalDateTime acceptanceDeadline) {
-        this.acceptanceDeadline = acceptanceDeadline;
-    }
-
-    public String getFailureCode() {
-        return failureCode;
-    }
-
-    public void setFailureCode(String failureCode) {
-        this.failureCode = failureCode;
-    }
-
-    public String getFailureMessage() {
-        return failureMessage;
-    }
-
-    public void setFailureMessage(String failureMessage) {
-        this.failureMessage = failureMessage;
-    }
+    public Money getAuthoritativeTotal() { return authoritativeTotal; }
+    public void setAuthoritativeTotal(Money authoritativeTotal) { this.authoritativeTotal = authoritativeTotal; }
+    public Long getCreditReservationId() { return creditReservationId; }
+    public void setCreditReservationId(Long creditReservationId) { this.creditReservationId = creditReservationId; }
+    public Long getTicketId() { return ticketId; }
+    public void setTicketId(Long ticketId) { this.ticketId = ticketId; }
+    public Long getAuthorizationId() { return authorizationId; }
+    public void setAuthorizationId(Long authorizationId) { this.authorizationId = authorizationId; }
+    public LocalDateTime getAcceptanceDeadline() { return acceptanceDeadline; }
+    public void setAcceptanceDeadline(LocalDateTime acceptanceDeadline) { this.acceptanceDeadline = acceptanceDeadline; }
+    public String getFailureCode() { return failureCode; }
+    public void setFailureCode(String failureCode) { this.failureCode = failureCode; }
+    public String getFailureMessage() { return failureMessage; }
+    public void setFailureMessage(String failureMessage) { this.failureMessage = failureMessage; }
 
     @Override
     public String toString() {
