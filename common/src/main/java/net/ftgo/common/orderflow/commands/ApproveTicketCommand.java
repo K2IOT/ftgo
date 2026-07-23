102f8168a9fc7040d449cf9cd36b9f52f37c4f43
KuuKuu <42238907+VODINHQUOC@users.noreply.github.com>
2026-07-23 14:06:38 +0700

    feat: carry ticket acceptance deadline

--- common/src/main/java/net/ftgo/common/orderflow/commands/ApproveTicketCommand.java
+++ common/src/main/java/net/ftgo/common/orderflow/commands/ApproveTicketCommand.java
@@ -2,15 +2,23 @@ package net.ftgo.common.orderflow.commands;
 
 import io.eventuate.tram.commands.common.Command;
 
+import java.time.LocalDateTime;
+
 public class ApproveTicketCommand implements Command {
 
     private Long ticketId;
+    private LocalDateTime acceptanceDeadline;
 
     public ApproveTicketCommand() {
     }
 
     public ApproveTicketCommand(Long ticketId) {
+        this(ticketId, null);
+    }
+
+    public ApproveTicketCommand(Long ticketId, LocalDateTime acceptanceDeadline) {
         this.ticketId = ticketId;
+        this.acceptanceDeadline = acceptanceDeadline;
     }
 
     public Long getTicketId() {
@@ -19,5 +27,13 @@ public class ApproveTicketCommand implements Command {
 
     public void setTicketId(Long ticketId) {
         this.ticketId = ticketId;
+    }
+
+    public LocalDateTime getAcceptanceDeadline() {
+        return acceptanceDeadline;
+    }
+
+    public void setAcceptanceDeadline(LocalDateTime acceptanceDeadline) {
+        this.acceptanceDeadline = acceptanceDeadline;
     }
 }
