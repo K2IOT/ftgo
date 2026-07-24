f826d95ecce40d8f1300d99bbb38bf4ca4d63a80
KuuKuu <42238907+VODINHQUOC@users.noreply.github.com>
2026-07-24 18:11:01 +0700

    feat: enable Consumer command result cache

--- consumer-service/src/main/java/net/ftgo/consumer/ConsumerServiceApplication.java
+++ consumer-service/src/main/java/net/ftgo/consumer/ConsumerServiceApplication.java
@@ -1,11 +1,14 @@
 package net.ftgo.consumer;
 
+import net.ftgo.common.messaging.IdempotentCommandConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
+@Import(IdempotentCommandConfiguration.class)
 public class ConsumerServiceApplication {
-    
+
     public static void main(String[] args) {
         SpringApplication.run(ConsumerServiceApplication.class, args);
     }
