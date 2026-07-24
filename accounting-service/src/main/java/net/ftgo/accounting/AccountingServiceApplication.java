a10cfe058b0aa1e8913f014cb7cd33c988815f91
KuuKuu <42238907+VODINHQUOC@users.noreply.github.com>
2026-07-24 18:11:23 +0700

    feat: enable Accounting command result cache

--- accounting-service/src/main/java/net/ftgo/accounting/AccountingServiceApplication.java
+++ accounting-service/src/main/java/net/ftgo/accounting/AccountingServiceApplication.java
@@ -1,11 +1,14 @@
 package net.ftgo.accounting;
 
+import net.ftgo.common.messaging.IdempotentCommandConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
+@Import(IdempotentCommandConfiguration.class)
 public class AccountingServiceApplication {
-    
+
     public static void main(String[] args) {
         SpringApplication.run(AccountingServiceApplication.class, args);
     }
