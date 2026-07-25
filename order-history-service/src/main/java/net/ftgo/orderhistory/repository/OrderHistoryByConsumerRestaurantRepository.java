21b0f7c50ce429d3756507e5d4a65e796dcdfdef
KuuKuu <42238907+VODINHQUOC@users.noreply.github.com>
2026-07-25 11:19:42 +0700

    feat: support since filter in restaurant bucket query

--- order-history-service/src/main/java/net/ftgo/orderhistory/repository/OrderHistoryByConsumerRestaurantRepository.java
+++ order-history-service/src/main/java/net/ftgo/orderhistory/repository/OrderHistoryByConsumerRestaurantRepository.java
@@ -8,6 +8,8 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
+
 @Repository
 public interface OrderHistoryByConsumerRestaurantRepository
     extends CassandraRepository<OrderHistoryByConsumerRestaurant, OrderHistoryByConsumerRestaurantKey> {
@@ -17,6 +19,15 @@ public interface OrderHistoryByConsumerRestaurantRepository
         Long consumerId,
         Long restaurantId,
         String creationMonth,
+        Pageable pageable
+    );
+
+    @Query("SELECT * FROM order_history_by_consumer_restaurant_v2 WHERE consumer_id = ?0 AND restaurant_id = ?1 AND creation_month = ?2 AND creation_date >= ?3")
+    Slice<OrderHistoryByConsumerRestaurant> findPageSince(
+        Long consumerId,
+        Long restaurantId,
+        String creationMonth,
+        LocalDateTime since,
         Pageable pageable
     );
 }
