7929033f8f0043dc82dad7b1797c9fb63fd4106a
KuuKuu <42238907+VODINHQUOC@users.noreply.github.com>
2026-07-25 11:37:14 +0700

    feat: enable consumer outbox metrics

--- consumer-service/src/main/java/net/ftgo/consumer/ConsumerServiceApplication.java
+++ consumer-service/src/main/java/net/ftgo/consumer/ConsumerServiceApplication.java
@@ -1,12 +1,13 @@
 package net.ftgo.consumer;
 
 import net.ftgo.common.messaging.IdempotentCommandConfiguration;
+import net.ftgo.common.messaging.OutboxMetricsConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
-@Import(IdempotentCommandConfiguration.class)
+@Import({IdempotentCommandConfiguration.class, OutboxMetricsConfiguration.class})
 public class ConsumerServiceApplication {
 
     public static void main(String[] args) {
