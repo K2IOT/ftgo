499d6c2d3ef62aefdae9c058f3b69a71e4eceae8
KuuKuu <42238907+VODINHQUOC@users.noreply.github.com>
2026-07-25 17:27:10 +0700

    test: provide kafka template mock for delivery pickup snapshot

--- delivery-service/src/test/java/net/ftgo/delivery/messaging/DeliveryPickupAddressIntegrationTest.java
+++ delivery-service/src/test/java/net/ftgo/delivery/messaging/DeliveryPickupAddressIntegrationTest.java
@@ -11,6 +11,8 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.test.context.ActiveProfiles;
 
 import java.math.BigDecimal;
@@ -23,6 +25,9 @@ import static org.assertj.core.api.Assertions.assertThat;
 @SpringBootTest
 @ActiveProfiles("test")
 class DeliveryPickupAddressIntegrationTest {
+
+    @MockBean
+    private KafkaTemplate<Object, Object> kafkaTemplate;
 
     @Autowired
     private OrderEventConsumer orderEventConsumer;
