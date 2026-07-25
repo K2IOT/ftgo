501ccb44fe8d643bfdba34f4a6ad1cbf8ef9eb1b
KuuKuu <42238907+VODINHQUOC@users.noreply.github.com>
2026-07-25 11:37:32 +0700

    feat: enable kitchen outbox metrics

--- kitchen-service/src/main/java/net/ftgo/kitchen/KitchenServiceApplication.java
+++ kitchen-service/src/main/java/net/ftgo/kitchen/KitchenServiceApplication.java
@@ -1,6 +1,7 @@
 package net.ftgo.kitchen;
 
 import net.ftgo.common.messaging.IdempotentCommandConfiguration;
+import net.ftgo.common.messaging.OutboxMetricsConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
@@ -9,7 +10,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 /** Kitchen Service entry point. */
 @SpringBootApplication
 @EnableScheduling
-@Import(IdempotentCommandConfiguration.class)
+@Import({IdempotentCommandConfiguration.class, OutboxMetricsConfiguration.class})
 public class KitchenServiceApplication {
 
     public static void main(String[] args) {
