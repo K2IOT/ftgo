89436511d8d93761ec414d457c7f0cff93856c50
KuuKuu <42238907+VODINHQUOC@users.noreply.github.com>
2026-07-25 11:17:14 +0700

    feat: add consumer bucket query repository

---
+++ order-history-service/src/main/java/net/ftgo/orderhistory/repository/OrderHistoryByConsumerRepository.java
@@ -0,0 +1,17 @@
+package net.ftgo.orderhistory.repository;
+
+import net.ftgo.orderhistory.domain.OrderHistoryByConsumer;
+import net.ftgo.orderhistory.domain.OrderHistoryByConsumerKey;
+import org.springframework.data.cassandra.repository.CassandraRepository;
+import org.springframework.data.cassandra.repository.Query;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrderHistoryByConsumerRepository
+    extends CassandraRepository<OrderHistoryByConsumer, OrderHistoryByConsumerKey> {
+
+    @Query("SELECT * FROM order_history_by_consumer_v2 WHERE consumer_id = ?0 AND creation_month = ?1")
+    Slice<OrderHistoryByConsumer> findPage(Long consumerId, String creationMonth, Pageable pageable);
+}
