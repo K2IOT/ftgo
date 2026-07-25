726a20d39b54512b80b50a2c648a95f68539c957
KuuKuu <42238907+VODINHQUOC@users.noreply.github.com>
2026-07-25 11:08:36 +0700

    fix: use Spring Cassandra primary key type package

--- order-history-service/src/main/java/net/ftgo/orderhistory/domain/PendingOrderEventKey.java
+++ order-history-service/src/main/java/net/ftgo/orderhistory/domain/PendingOrderEventKey.java
@@ -1,9 +1,9 @@
 package net.ftgo.orderhistory.domain;
 
 import org.springframework.data.cassandra.core.cql.Ordering;
+import org.springframework.data.cassandra.core.cql.PrimaryKeyType;
 import org.springframework.data.cassandra.core.mapping.PrimaryKeyClass;
 import org.springframework.data.cassandra.core.mapping.PrimaryKeyColumn;
-import org.springframework.data.cassandra.core.mapping.PrimaryKeyType;
 
 import java.io.Serializable;
 import java.time.Instant;
