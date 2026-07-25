8810b058fb0ea1a73db1b37ffe3dfc53ae594c52
KuuKuu <42238907+VODINHQUOC@users.noreply.github.com>
2026-07-25 11:37:49 +0700

    feat: enable accounting outbox metrics

--- accounting-service/src/main/java/net/ftgo/accounting/AccountingServiceApplication.java
+++ accounting-service/src/main/java/net/ftgo/accounting/AccountingServiceApplication.java
@@ -1,12 +1,13 @@
 package net.ftgo.accounting;
 
 import net.ftgo.common.messaging.IdempotentCommandConfiguration;
+import net.ftgo.common.messaging.OutboxMetricsConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
-@Import(IdempotentCommandConfiguration.class)
+@Import({IdempotentCommandConfiguration.class, OutboxMetricsConfiguration.class})
 public class AccountingServiceApplication {
 
     public static void main(String[] args) {
