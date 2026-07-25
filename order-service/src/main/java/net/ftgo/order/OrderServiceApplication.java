44ba5737a7ab2b1491dee7b8ce98e61affadc594
KuuKuu <42238907+VODINHQUOC@users.noreply.github.com>
2026-07-25 11:38:05 +0700

    feat: enable order outbox metrics

--- order-service/src/main/java/net/ftgo/order/OrderServiceApplication.java
+++ order-service/src/main/java/net/ftgo/order/OrderServiceApplication.java
@@ -1,11 +1,14 @@
 package net.ftgo.order;
 
+import net.ftgo.common.messaging.OutboxMetricsConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
+@Import(OutboxMetricsConfiguration.class)
 public class OrderServiceApplication {
 
     public static void main(String[] args) {
