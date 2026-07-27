6da44790f6e73277b879cab1393688051c0bc952
KuuKuu <42238907+VODINHQUOC@users.noreply.github.com>
2026-07-27 09:38:33 +0700

    feat(common): add settlement identity to cancellation command

--- common/src/main/java/net/ftgo/common/orderflow/commands/ReverseAuthorizationCommand.java
+++ common/src/main/java/net/ftgo/common/orderflow/commands/ReverseAuthorizationCommand.java
@@ -6,28 +6,35 @@ public class ReverseAuthorizationCommand implements Command {
 
     private Long consumerId;
     private Long authorizationId;
+    private Long orderId;
+    private String requestId;
 
     public ReverseAuthorizationCommand() {
     }
 
+    /** Legacy constructor retained for rolling compatibility. */
     public ReverseAuthorizationCommand(Long consumerId, Long authorizationId) {
+        this(consumerId, authorizationId, null, null);
+    }
+
+    public ReverseAuthorizationCommand(
+        Long consumerId,
+        Long authorizationId,
+        Long orderId,
+        String requestId
+    ) {
         this.consumerId = consumerId;
         this.authorizationId = authorizationId;
+        this.orderId = orderId;
+        this.requestId = requestId;
     }
 
-    public Long getConsumerId() {
-        return consumerId;
-    }
-
-    public void setConsumerId(Long consumerId) {
-        this.consumerId = consumerId;
-    }
-
-    public Long getAuthorizationId() {
-        return authorizationId;
-    }
-
-    public void setAuthorizationId(Long authorizationId) {
-        this.authorizationId = authorizationId;
-    }
+    public Long getConsumerId() { return consumerId; }
+    public void setConsumerId(Long consumerId) { this.consumerId = consumerId; }
+    public Long getAuthorizationId() { return authorizationId; }
+    public void setAuthorizationId(Long authorizationId) { this.authorizationId = authorizationId; }
+    public Long getOrderId() { return orderId; }
+    public void setOrderId(Long orderId) { this.orderId = orderId; }
+    public String getRequestId() { return requestId; }
+    public void setRequestId(String requestId) { this.requestId = requestId; }
 }
