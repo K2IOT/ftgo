261238853a34232db88a33d4d071084d3412b9bf
KuuKuu <42238907+VODINHQUOC@users.noreply.github.com>
2026-07-27 16:12:53 +0700

    test: expose consumer security probe controller

--- consumer-service/src/test/java/net/ftgo/consumer/config/SecuritySmokeTest.java
+++ consumer-service/src/test/java/net/ftgo/consumer/config/SecuritySmokeTest.java
@@ -66,7 +66,7 @@ class SecuritySmokeTest {
     }
 
     @RestController
-    static class ProbeController {
+    public static class ProbeController {
 
         @GetMapping("/consumers/security-probe")
         String consumerProbe() {
