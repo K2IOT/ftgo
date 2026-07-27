5ef56231b9d778291616293b671a00067778cbd9
KuuKuu <42238907+VODINHQUOC@users.noreply.github.com>
2026-07-27 16:12:23 +0700

    test: expose order security probe controller

--- order-service/src/test/java/net/ftgo/order/config/SecuritySmokeTest.java
+++ order-service/src/test/java/net/ftgo/order/config/SecuritySmokeTest.java
@@ -66,7 +66,7 @@ class SecuritySmokeTest {
     }
 
     @RestController
-    static class ProbeController {
+    public static class ProbeController {
 
         @GetMapping("/orders/security-probe")
         String orderProbe() {
