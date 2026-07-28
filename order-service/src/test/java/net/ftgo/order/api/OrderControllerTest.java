5f83829fed503e5fd2e33d7e93888039c72ccdcb
KuuKuu <42238907+VODINHQUOC@users.noreply.github.com>
2026-07-28 12:16:22 +0700

    style: remove duplicate order test import

--- order-service/src/test/java/net/ftgo/order/api/OrderControllerTest.java
+++ order-service/src/test/java/net/ftgo/order/api/OrderControllerTest.java
@@ -39,7 +39,6 @@ import static org.mockito.Mockito.verifyNoInteractions;
 import static org.mockito.Mockito.when;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.authentication;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.authentication;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
