06c99c6f0c537a6b6f684a7bb02b31a05567b153
KuuKuu <42238907+VODINHQUOC@users.noreply.github.com>
2026-08-10 10:35:52 +0700

    test: wire reconciliation queue into shared contracts

--- order-service/src/test/java/net/ftgo/order/saga/CreateOrderSagaSharedContractTest.java
+++ order-service/src/test/java/net/ftgo/order/saga/CreateOrderSagaSharedContractTest.java
@@ -8,6 +8,7 @@ import net.ftgo.accounting.payment.PaymentAuthorizationGateway;
 import net.ftgo.accounting.repository.AccountRepository;
 import net.ftgo.accounting.settlement.PaymentLedgerService;
 import net.ftgo.accounting.settlement.SettlementGateway;
+import net.ftgo.accounting.settlement.SettlementReconciliationWorkRepository;
 import net.ftgo.common.Money;
 import net.ftgo.common.channels.ChannelNames;
 import net.ftgo.common.messaging.IdempotentCommandExecutor;
@@ -67,6 +68,7 @@ class CreateOrderSagaSharedContractTest {
             mock(PaymentAuthorizationGateway.class),
             mock(SettlementGateway.class),
             mock(PaymentLedgerService.class),
+            mock(SettlementReconciliationWorkRepository.class),
             mock(IdempotentCommandExecutor.class)
         ).commandHandlers();
 
