56ee0668897e1f5eaeabb3b35bd9be5a0b42aa5d
KuuKuu <42238907+VODINHQUOC@users.noreply.github.com>
2026-08-11 13:55:47 +0700

    test: migrate Delivery integration Mockito override

--- delivery-service/src/test/java/net/ftgo/delivery/DeliveryServiceIntegrationTest.java
+++ delivery-service/src/test/java/net/ftgo/delivery/DeliveryServiceIntegrationTest.java
@@ -8,9 +8,9 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 import java.time.LocalDateTime;
 
@@ -24,7 +24,7 @@ import static org.junit.jupiter.api.Assertions.*;
 @ActiveProfiles("test")
 class DeliveryServiceIntegrationTest {
 
-    @MockBean
+    @MockitoBean
     private KafkaTemplate<Object, Object> kafkaTemplate;
 
     @Autowired
