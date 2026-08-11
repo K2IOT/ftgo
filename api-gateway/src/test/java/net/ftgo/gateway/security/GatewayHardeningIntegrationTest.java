dbd3e6234bc6bf0adeff97463b26528f60788e59
KuuKuu <42238907+VODINHQUOC@users.noreply.github.com>
2026-08-11 11:04:16 +0700

    test: replace MockBean with Spring 7 MockitoBean

--- api-gateway/src/test/java/net/ftgo/gateway/security/GatewayHardeningIntegrationTest.java
+++ api-gateway/src/test/java/net/ftgo/gateway/security/GatewayHardeningIntegrationTest.java
@@ -3,7 +3,6 @@ package net.ftgo.gateway.security;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.ApplicationContext;
 import org.springframework.http.HttpHeaders;
 import org.springframework.mock.http.server.reactive.MockServerHttpRequest;
@@ -11,6 +10,7 @@ import org.springframework.mock.web.server.MockServerWebExchange;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.security.oauth2.jwt.ReactiveJwtDecoder;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import reactor.core.publisher.Mono;
 
@@ -30,7 +30,7 @@ class GatewayHardeningIntegrationTest {
     @Autowired
     private ApplicationContext applicationContext;
 
-    @MockBean
+    @MockitoBean
     private ReactiveJwtDecoder jwtDecoder;
 
     @Test
